65358ac1f4da9c6bde089aea2211ea8d10063517
Colin Thomas-Arnold <colinta@mac.com>
2012-01-17 16:01:28 -0700

    support for quickfinding multiple selections.

--- quick_find.py
+++ quick_find.py
@@ -3,54 +3,77 @@ import sublime_plugin
 
 
 class QuickfindCommand(sublime_plugin.TextCommand):
-    def run(self, edit, look_backwards=False, use_regex=None, wrap=True, case_insensitive=True):
-        region = self.view.sel()[0]
+    def quickfind(self, search, region, look_backwards=False, use_regex=None, wrap=True, case_insensitive=True):
+        if not len(search):
+            return
 
-        def on_change(search):
-            if not len(search):
-                return
+        flags = 0
+        if not use_regex:
+            flags |= sublime.LITERAL
 
-            if use_regex:
-                flags = 0
-            else:
-                flags = sublime.LITERAL
+        if case_insensitive:
+            flags |= sublime.IGNORECASE
 
-            if case_insensitive:
-                flags |= sublime.IGNORECASE
-
-            if look_backwards:
-                start = region.begin()
-                point = start - 1
-                while point >= 0:
-                    found = self.view.find(search, point, flags)
-                    if found and found.end() < start:
-                        break
-                    else:
-                        found = None
+        if look_backwards:
+            start = region.begin()
+            point = start - 1
+            while point >= 0:
+                found = self.view.find(search, point, flags)
+                if found and found.end() < start:
+                    break
+                else:
+                    found = None
+                point -= 1
+                # if we get to the beginning, and wrap is enabled, start at the end and keep searching
+                # unless we already tried that
+                if point < 0 and wrap and start < self.view.size():
+                    start = point = self.view.size()
                     point -= 1
-                    # if we get to the beginning, and wrap is enabled, start at the end and keep searching
-                    # unless we already tried that
-                    if point < 0 and wrap and start < self.view.size():
-                        start = point = self.view.size()
-                        point -= 1
-            else:
-                start = region.end()
-                found = self.view.find(search, start, flags)
-                if not found and wrap:
-                    found = self.view.find(search, 0, flags)
+        else:
+            start = region.end()
+            found = self.view.find(search, start, flags)
+            if not found and wrap:
+                found = self.view.find(search, 0, flags)
 
-            if found:
+        return found
+
+    def run(self, edit, **kwargs):
+        regions = [region for region in self.view.sel()]
+
+        # any edits that are performed will happen in reverse; this makes it
+        # easy to keep region.a and region.b pointing to the correct locations
+        def compare(region_a, region_b):
+            return cmp(region_b.end(), region_a.end())
+        regions.sort(compare)
+        first_region = regions[0]
+
+        def on_change_each(search):
+            new_regions = filter(bool, (self.quickfind(search, region, **kwargs) for region in regions))
+
+            if new_regions:
+                if len(new_regions) > 1:
+                    sublime.status_message('Found %i instances of "%s"' % (len(new_regions), search))
+                else:
+                    sublime.status_message('Found "%s"' % search)
+
                 selection = self.view.sel()
                 selection.clear()
-                selection.add(found)
-                sublime.status_message('Found "%s"' % search)
-                self.view.show_at_center(found)
+                for region in new_regions:
+                    selection.add(region)
+                self.view.show_at_center(region)
             else:
                 sublime.status_message('Could not find "%s"' % search)
 
         cmd, _, _ = self.view.command_history(0, True)
-        if not region.empty() and cmd == 'quickfind':
-            on_change(self.view.substr(region))
+        selection = self.view.substr(first_region)
+        if selection and any([selection != self.view.substr(region) for region in regions]):
+            selection = None
+
+        if selection and cmd == 'quickfind':
+            on_change_each(selection)
         else:
-            prompt = 'Search' if not use_regex else "Regex Search"
-            self.view.window().show_input_panel(prompt, self.view.substr(region), None, on_change, None)
+            if 'use_regex' in kwargs and kwargs['use_regex']:
+                prompt = "Regex Search"
+            else:
+                prompt = "Search"
+            self.view.window().show_input_panel(prompt, self.view.substr(first_region), None, on_change_each, None)
