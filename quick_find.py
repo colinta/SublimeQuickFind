224020c9f7e4b168aa0b7f19f3dd9660e0d7cc0b
Colin T.A. Gray <colinta@gmail.com>
2018-10-12 13:07:30 -0400

    fixes viewport in latest ST3, and better anyway

--- quick_find.py
+++ quick_find.py
@@ -79,11 +79,7 @@ class QuickfindCommand(sublime_plugin.TextCommand):
                     selection.add(region)
 
                 pos = self.view.viewport_position()
-                self.view.show_at_center(region)
-                new_pos = self.view.viewport_position()
-                if abs(new_pos[0] - pos[0]) <= 1.0 and abs(new_pos[1] - pos[1]) <= 1.0:
-                    self.view.set_viewport_position((new_pos[0], new_pos[1] + 1))
-                    self.view.set_viewport_position((new_pos[0], new_pos[1]))
+                self.view.show(region)
             else:
                 sublime.status_message('Could not find "%s"' % search)
 
